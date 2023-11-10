425d1594854a6bf3c73711e9c9e9f6013fc97249
nvsathyababu <nvsathyababu@altimetrik.com>
2023-11-10 21:52:46 +0530

    first commit

---
+++ src/test/java/com/javatpoint/SpringBootRestExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javatpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringBootRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(true,true);
+	}
+
+}
